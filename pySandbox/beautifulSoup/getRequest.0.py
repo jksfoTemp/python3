a92da0a974917486853a0f3b4e6c1461abff0659
jksfoTemp <josephkelly.resume@gmail.com>
2025-02-20 22:20:45 -0800

    Almost there, works, remaining:
    TODO: Check for output consolidation
    TODO: Move screen output to single location
    TODO: what is the type of obj? line ~74
    TODO: Implement logging
    TODO: I could change this to something like 'messaging' with an array of messages a \
      failure code and an exit code to handle the error messaging and exit in one place
    TODO: Last statement is the filename (on success)
    TODO: Replace all print statement to messaging()

--- pySandbox/beautifulSoup/getRequest.0.py
+++ pySandbox/beautifulSoup/getRequest.0.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-''' Only using this to download a save a sample page 
-  getRequest.0.py 'foo.com' 'foo.txt' 
-    # def main(url='', outputFile='', verbose=False):
-      # url = 'https://en.wikipedia.org/wiki/foo' 
-      # outputFile = 'sampleFile.html' 
-  Output file: sampleFile.html
-  
-  TODO: Parse with PyLance (remove other linters)
-  TODO: Implement variable typing
-  TODO: Set up a decent formatter that actually works (unlike black)
-  TODO: Implement logging 
+"""Only using this to download a save a sample page
+getRequest.0.py 'foo.com' 'foo.txt'
+  # def main(url='', outputFile='', verbose=False):
+    # url = 'https://en.wikipedia.org/wiki/foo'
+    # outputFile = 'sampleFile.html'
+Output file: sampleFile.html
 
-'''
+TODO: Parse with PyLance (remove other linters)
+TODO: Implement variable typing
+TODO: Set up a decent formatter that actually works (unlike black)
+TODO: Implement logging
+TODO: Set up debugging
+
+Notes:
+* Use vscode, not vscodium  (vscodium doesn't have the same features)
+* Use # %% to run a cell in vscode
+
+"""
 
 import requests
-import os 
-import sys 
+import os
+import sys
 
-# Removed - was just being used for testing 
+# Removed - was just being used for testing
 # class Stubb(object):
 #   def __init__(self, status_code, content):
 #     self._status_code = status_code
@@ -29,94 +34,119 @@ import sys
 #     return self._status_code, self._content
 
 
-def getURLObj(urlT): 
-  try: 
-    # REMOVEME
-    # r = stubb (200, "foo")
-    # print (r.status_code)
-    # print (r.content)
-    
-    r = requests.get(
-         urlT 
-    )
-    if r.status_code == 200:
-      print ("Got it: {r.status_code}")
-      return r.content
-    else: 
-      print ("Error getting url: {urlT}  Response status code: \
-        {r.status_code)}") 
-      return None 
+def getURLObj(urlT):
+    """_summary_
 
-  except requests.exception.RequestException as e: 
-    print ("Error getting url: {urlT};  Response status code: \
-      {r.status_code}") 
-    return None 
+    Args:
+        urlT (_type_): _description_
 
+    Returns:
+        _type_: _description_
+    """
+    try:
+        # REMOVEME
+        # r = stubb (200, "foo")
+        # print (r.status_code)
+        # print (r.content)
 
-def writeObj(obj, outFile): 
-  if obj is None: # Handle case where no content was retrieved
-    print(f"No content to write to file: {obj} \n {outFile}")
-    return
+        r = requests.get(urlT)
+        code = r.status_code
+        if code == 200:
+            print(f"Got it: {code}")
+            return r.content
+        else:
+            print(
+                f"Error getting url: {urlT}  Response status code: \
+                  {code}"
+            )
+            return None
 
-  try: 
-    with open(outFile, "wt") as f:
-      # f.write(str(obj.content)) # bad 
-      f.write(obj.decode('utf-8')) # Decode bytes to string before writing 
-    
-  except Exception as e:
-    print(f"An error occurred writing the file: {e}")
+    except requests.exception.RequestException as e:
+        print(
+            f"Error getting url: {urlT};  Response status code: \
+            {code}"
+        )
+        return None
 
 
-# def main(url='https://en.wikipedia.org/wiki/foo', 
+def writeObj(obj, outFile):
+    """Handle writing the object to a file."""
+    if obj is None:  # Handle case where no content was retrieved
+        print(f"No content to write to file: {obj} \n {outFile}")
+        return
+
+    try:
+        with open(outFile, "wt") as f:
+            # f.write(str(obj.content)) # bad
+            f.write(obj.decode("utf-8"))  # Decode bytes to string before writing
+
+    except Exception as e:
+        print(f"An error occurred writing the file: {e}")
+
+
+# def main(url='https://en.wikipedia.org/wiki/foo',
 #  outputFile='sampleFile.html', verbose=False):
-def main( \
-  url="https://en.wikipedia.org/wiki/foo", \
-    outputFile="sampleFile.html", \
-      verbose=False):
-  try:   
-    obj = getURLObj(url) # verbose 
-    if obj: # Only write if content was successfully retrieved
-      writeObj(obj, outputFile)
-      print ("Should be good. \n \t URL: {url} \
-        \t File: {outputFile} \
-          \t Output: {verbose}")
-      return_status = 0 
-    else: 
-      print ('Failed to retrieve content from URL.')
-      return_status = -1 
-  except Exception as e:
-    print(f"An error occurred in main: {e}")
-    return_status = 1
-  finally:
-    sys.exit(return_status) # Exit with the determined status code
+def main(
+    url="https://en.wikipedia.org/wiki/foo", outputFile="sampleFile.html", verbose=False
+):
+    try:
+        obj = getURLObj(url)  # verbose
+        if obj:  # Only write if content was successfully retrieved
+            writeObj(obj, outputFile)
+
+            ### TODO:  output is not being decoded
+            ### TODO:  validate inputs, return go/no-go status and error message (array?)
+
+            print(
+                f"Should be good. \n \t URL: {url} \
+                \n\t File: {outputFile} \
+                \n\t Output: {verbose}"
+            )
+            return_status = 0
+        else:
+            print("Failed to retrieve content from URL.")
+            return_status = -1
+    except Exception as e:
+        print(f"An error occurred in main: {e}")
+        return_status = 1
+    finally:
+        sys.exit(return_status)  # Exit with the determined status code
 
 
-if __name__ == "__main__": \
-  # bad syntax (list of arguments instead of individual arguments)
-  # main(sys.argv[0], sys.argv[1], sys.argv[2]) 
-  if len(sys.argv) == 4: # Check for correct number of arguments
-    url_arg = sys.argv[1]
-    output_file_arg = sys.argv[2]
-    verbose_arg = sys.argv[3]
-    main(url_arg, output_file_arg, verbose_arg)
-  elif len(sys.argv) == 3: 
-    url_arg = sys.argv[1]
-    output_file_arg = sys.argv[2]
-    main(url_arg, output_file_arg)
-  elif len(sys.argv) == 2: # Allow URL only, use default output file
-    url_arg = sys.argv[1]
-    main(url_arg)
-  elif len(sys.argv) == 1: # Allow URL only, use default output file
-    main(url_arg)
-  else:
-    print("Usage: getRequest.0.py 'URL_to_download' 'output_file.html' \
-       [TRUE/FALSE] \
-        (\t getRequest.0.py {sys.argv[1]}, {sys.argv[2]}, {sys.argv[3]})")
-    print("       getRequest.0.py 'URL_to_download' \
-      (\t getRequest.0.py {sys.argv[1]}, {sys.argv[2]})")
-    print("       getRequest.0.py 'URL_to_download' \
-      (\t getRequest.0.py {sys.argv[1]})")
-    print("       getRequest.0.py \
-      (\t getRequest.0.py)")
-    print("Error: Incorrect number of arguments.")
-
+if __name__ == "__main__":
+    url_arg = ""
+    # Should input validation be done here or in main?
+    # main(sys.argv[0], sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 4:  # Check for correct number of arguments
+        url_arg = sys.argv[1]
+        output_file_arg = sys.argv[2]
+        verbose_arg = sys.argv[3]
+        main(url_arg, output_file_arg, verbose_arg)
+    elif len(sys.argv) == 3:
+        url_arg = sys.argv[1]
+        output_file_arg = sys.argv[2]
+        main(url_arg, output_file_arg)
+    elif len(sys.argv) == 2:  # Allow URL only, use default output file
+        url_arg = sys.argv[1]
+        main(url_arg)
+    elif len(sys.argv) == 1:  # Allow URL only, use default output file
+        main()
+    else:
+        print(
+            "Usage: getRequest.0.py 'URL_to_download' 'output_file.html' \
+            [TRUE/FALSE] \
+            (\t getRequest.0.py {sys.argv[1]}, {sys.argv[2]}, {sys.argv[3]})"
+        )
+        print(
+            "\tgetRequest.0.py 'URL_to_download' 'output_file.html' \
+            (\t getRequest.0.py {sys.argv[1]}, {sys.argv[2]})"
+        )
+        print(
+            "\tgetRequest.0.py 'URL_to_download' \
+            (\t getRequest.0.py {sys.argv[1]})"
+        )
+        print(
+            "\tgetRequest.0.py \
+            (\t getRequest.0.py)"
+        )
+        print("Error: Incorrect number of arguments.")

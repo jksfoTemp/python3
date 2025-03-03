274f67d42d00a2f8e50c1860e7cc8af63f1c0f50
jksfoTemp <josephkelly.resume@gmail.com>
2025-03-03 09:28:20 -0800

    Yes, it works. Have not checked command line arguments but happy for now. About to do some cleanup of source.

--- wget2/getRequest.0.py
+++ wget2/getRequest.0.py
@@ -2,24 +2,22 @@
 # -*- coding: utf-8 -*-
 
 # region Description
-"""Only using this to download a save a sample page
-
+"""
+Only using this to download a save a sample page
 Yes, I could have used wget, but I wanted to do this in Python 
-
 Remember YAGNI and KISS
-
-getRequest.0.py 'foo.com' 'foo.txt'
-  # def main(url='', outputFile='', verbose=False):
-    # url = 'https://en.wikipedia.org/wiki/foo'
-    # outputFile = 'sampleFile.html'
-Output file: sampleFile.html
-
-TODO: Get it to run again and then clean it up 
 
 # https://www.postjobfree.com/jobs?q=data+entry&n=&t=&c=&l=San+Francisco%2C+CA&radius=2&r=100 
 
-TODO: DONE Rearrange the code to be more modular and use regions 
-TODO: DONE Set up a decent formatter that actually works (unlike black)
+TO
+Notes:
+* Use # %% to run a jupyter cell in vscode
+* Consider (using/transforming) this to be a library for other projects
+"""
+# endregion Description
+
+# region TODO
+"""
 TODO: Implement logging
 TODO: Last statement is the filename (on success)
 TODO: Check for output consolidation
@@ -29,198 +27,136 @@ TODO: I could change this to something like 'messaging' with an array of message
   failure code and an exit code to handle the error messaging and exit in one place
 TODO: Check that comments are correct
 TODO: Avoid returning 'None' from a function - see notes and generally check for nulls 
-
-Notes:
-* Use # %% to run a cell in vscode
-* Consider (using/transforming) this to be a library for other projects
 """
-# endregion Description
+# endregion TODO 
+
+# region Imports
 
 import requests
 import os
 import sys
 import logging
-# from xmlrpc.client import Boolean
 from urllib.parse import urlparse
-from requests.compat import urlparse as requests_urlparse
 from typing import NoReturn  # , ReadOnly
 
 # endregion Imports
 
-"""_summary_
-
-    Raises:
-        Exception: _description_
-
-    Returns:
-        _type_: _description_
-"""
-
 # region Variables
+
 VERBOSE: bool = True  # Global verbose flag for logging
 STDOUT: bool = True  # Global flag for screen output
 FILENAME: str = "FILENAME - set in init function"
 URL: str = "URL - set in init function"
-OUPUTFILE: str = "OUPUTFILE - set in init function"
+OUPUTFILE2: str = "OUPUTFILE2 - set in init function"
 
-# todo: set up a global log file WITH FILENAME ARG, FIRST VALUE
-LOG_FILE: str = "app.log"  # Global log file name for logging output
-LOG_LEVEL: int = (
-    logging.DEBUG
-)  # Global log level for logging output (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-LOG_FORMAT: str = (
-    "%(asctime)s - %(levelname)s - %(message)s"  # Global log format for logging output
-)
-LOG_DATE_FORMAT: str = (
-    "%m/%d/%Y %I:%M:%S %p"  # Global log date format for logging output  # 06/24/2018 12:00:00 PM  # '%Y-%m-%d %H:%M:%S' # '%m/%d/%Y %I:%M:%S %p'  # 06/24/2018 12:00:00 PM  # '%Y-%m-%d %H:%M:%S'
-)
 # endregion Variables
 
+# region Log Settings 
+LOG_LEVEL: int = (logging.INFO
+)  # Global log level for logging output (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+LOG_FORMAT: str = ("%(asctime)s - %(levelname)s - %(message)s")  # Global log format for log output
+LOG_DATE_FORMAT: str = (    
+    "%m/%d/%Y %I:%M:%S %p"  # Global log date format for logging output  # 06/24/2018 12:00:00 PM  # '%Y-%m-%d %H:%M:%S' # '%m/%d/%Y %I:%M:%S %p'  # 06/24/2018 12:00:00 PM  # '%Y-%m-%d %H:%M:%S'
+)
 
-# region Logging
-# logging.basicConfig(level=logging.DEBUG)
-# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# endregion
+# endregion Log Settings 
 
 
-# region Messaging
-def messaging(msg: str):
-    """_summary_
-    This is a placeholder for a more sophisticated messaging system
+# region Input Validation
+    """ 
+      NOT USED 
+      # def validateInput(_url: str, _outputFile: str, _verbose: bool) -> int:
+      # 
+      # _summary_
+      #       main already checks for the correct number of arguments, so \
+      #         this is just to validate the inputs and types 
+
+      #     Args:
+      #         _url (_type_): _description_
+      #         _outputFile (_type_): _description_
+      #         _verbose (_type_): _description_
+      #     """
+
+      #     # Call to validate URL
+      #     # is_valid = is_valid_url(str(url) if url is not None else "")
+      #     if not _url:
+      #         # print(f"Error: URL is required.  URL: {_url}")
+      #         return 11
+      #     if not is_valid_url(str(_url)):
+      #         # print(f"Error: URL appears to be invalid.  URL: {_url}")
+      #         return 12
+      #     if not _outputFile:
+      #         # print(f"Error: Output file is required.  Output file: {_outputFile}")
+      #         return 21
+      #     if _verbose not in [True, False]:
+      #         # print(f"Error: Verbose must be True or False.  Verbose: {_verbose}")
+      #         return 31
+      #     # passed
+      #     return 0
     """
-    print(
-        msg
-        # logging.debug(msg)
-    )  # Debugging message
-    # logging.info(msg)  # Informational message
-    
-    # It doesn't like the type of NoReturn
-    # def displayInputError(_isValid: int, _url: str, _outputFile: str, _verbose: bool) -> NoReturn:
-    #   def displayInputError(
-    #     _isValid: int, _url: str, _outputFile: str, _verbose: bool
-    # ) -> None
-
-    """_summary_
-    The general idea is to display the error message and exit the program all in one place
-    and not confuse the business logic and error handling with the presentation logic
-    
-    TODO: I could change this to something like 'messaging' with an array of messages a \
-      failure code and an exit code to handle the error messaging and exit in one place
-    """
-
-    # huh stucks for compilation
-    _isValid: int = 0
-    _url: str = ""
-    _outputFile: str = ""
-    _verbose: bool = False
-
-    if _isValid == 11:
-        print(f"Error: URL is required.  URL: {_url}")
-    elif _isValid == 12:
-        print(f"Error: URL appears to be invalid.  URL: {_url}")
-    elif _isValid == 21:
-        print(f"Error: Output file is required.  Output file: {_outputFile}")
-    elif _isValid == 31:
-        print(f"Error: Verbose must be True or False.  Verbose: {_verbose}")
-    else:
-        print(f"Error: Unknown error occurred.  Error code: {_isValid}")
-
-
-# endregion Messaging
-
-
-# region Error Handling
-def handle_error(e: Exception):
-    """_summary_
-    This is a placeholder for a more sophisticated error handling system
-    """
-    print(f"An error occurred: {e}")
-    # logging.error
-    # logging.error(f"An error occurred: {e}")
-    
-    # def validateInput(_url: str, _outputFile: str, _verbose: bool) -> int:
-
-
-def validateInput(_url: str, _outputFile: str, _verbose: bool) -> int:
-    """_summary_
-      main already checks for the correct number of arguments, so \
-        this is just to validate the inputs and types 
-
-    Args:
-        _url (_type_): _description_
-        _outputFile (_type_): _description_
-        _verbose (_type_): _description_
-    """
-
-    # Call to validate URL
-    # is_valid = is_valid_url(str(url) if url is not None else "")
-    if not _url:
-        # print(f"Error: URL is required.  URL: {_url}")
-        return 11
-    if not is_valid_url(str(_url)):
-        # print(f"Error: URL appears to be invalid.  URL: {_url}")
-        return 12
-    if not _outputFile:
-        # print(f"Error: Output file is required.  Output file: {_outputFile}")
-        return 21
-    if _verbose not in [True, False]:
-        # print(f"Error: Verbose must be True or False.  Verbose: {_verbose}")
-        return 31
-    # passed
-    return 0
-
 
 # endregion Input Validation
 
-
 # region Business Logic
+
 def getURLObj(urlT: str) -> requests.Response:
-    """_summary_
-    Args:
-        urlT (_type_): _description_
-    Returns:
-        _type_: _description_
-    """
+    # """
+    # _summary_
+    # Args:
+    #     urlT (_type_): _description_
+    # Returns:
+    #     _type_: _description_
+    # """
+    logger.info("Enter getURLObj")
+
     code: int = -1
     r: requests.Response = requests.Response()
     try:
         r = requests.get(urlT)
         code = r.status_code
         if code == 200:
-            print(f"Got it: {code}")
+            logger.info(f"Got it: {code}")
+            logger.info(str({r}))
         else:
-            print(
-                f"Error getting url: {urlT}  Response status code: \
-                  {code}"
+            logger.error(
+                f"Error getting url: {urlT}  Response status code: {code}"
             )
     except requests.RequestException as e:
-        print(
-            f"Error getting url: {urlT};  Response status code: \
-            str({code})"
+        logger.error(
+            f"Error getting url: {urlT};  Response status code: {code}"
         )
+    logger.info("Exit getURLObj")
     return r  # Return the response object
 
+# endregion Business Logic
 
+# region File Handling 
 def writeObj(obj: requests.Response, outFile: str):
+
+    logger.info("Enter writeObj")
 
     try:
         """Handle writing the object to a file."""
         if obj is None:  # Handle case where no content was retrieved
             # TODO: Move screen output to single location
-            print(f"No content to write to file: {obj} \n {outFile}")
+            logger.warning(f"No content to write to file: {obj} \n {outFile}")
             return
-            with open(outFile, "wt") as f:
-                # f.write(str(obj.content)) # bad
-                f.write(
-                    obj.content.decode("utf-8")
-                )  # Decode bytes to string before writing
+        with open(outFile, "wt") as f:
+            # f.write(str(obj.content)) # bad
+            f.write(
+                obj.content.decode("utf-8")
+             )  # Decode bytes to string before writing
 
     except Exception as e:
-        print(f"An error occurred writing the file: {e}")
+        logger.error("Error writeObj ... ")
+        logger.error(f"An error occurred writing the file: {e}")
+    finally:
+        logger.info("Exit writeObj")
 
+# endregion File Handling
 
-# Move me
+# region Valid 
+
 def is_valid_url(url_string: str) -> bool:
     """
     Checks if a string is likely a valid URL based on syntax,
@@ -241,19 +177,25 @@ def is_valid_url(url_string: str) -> bool:
         # parsed_url = urlparse(url_string)
         # for requests consistency
         parsed_url = requests_urlparse(url_string)
-        return all(
+        result = all(
+        result = all(
             [parsed_url.scheme, parsed_url.netloc]
         )  # Check for both scheme and netloc
     except:  # Catch potential parsing errors, consider more specific exception if needed
+        result = False
+    logger.info("Exit is_valid_url")
+    return result
         return False
-
+    logger.info("Exit is_valid_url")
+    logger.info("Exit is_valid_url")
 
 # endregion
 
-
 # region Main
+
 def main():
-    """_summary_"""
+    """
+    # _summary_ 
     # TODO: Consider changing these to a ReadOnly class, tuple or const - singleton, this if fine
     global VERBOSE
     global STDOUT
@@ -266,70 +208,57 @@ def main():
     # use a function for this and just globals, no need for a class. Could use a tuple
     # for url and output file but this is a singleton use case
 
+    LOG_FILE = sys.argv[0] + ".log"
+    OUPUTFILE2= sys.argv[0] + ".txt"
+
     logger.info("Main() Started.")
 
     try:
         if len(sys.argv) > 5:
             raise Exception("Too many arguments")
-
+    
         try:
             STDOUT = bool(sys.argv[4])
+
         except IndexError:
             STDOUT = False
+            logger.info("Main() output not specified.")
 
         try:
             VERBOSE = bool(sys.argv[3])
         except IndexError:
             VERBOSE = False
+            logger.info("Main() has four arguments.")
 
-        LOG_FILE = sys.argv[0] + ".log"
+        url = "https://www.postjobfree.com/jobs?q=data+entry&n=&t=&c=&l=San+Francisco%2C+CA&radius=2&r=100"
+      
+        # Should be good to go
+        # obj: requests.Response = getURLObj(url)
+        obj = getURLObj(url)
 
-    # chang to use globals , a cheat but makes sense for this purpose
-    # try:
-    #     outputFile: str = str(sys.argv[2]) + ".html"
-    # except:
-    #     outputFile = sys.argv[0] + ".html"
+        logger.info("In main ... ")
+        
+        if obj:  # Only write if content was successfully retrieved
+            writeObj(obj, OUPUTFILE2)
+            # Success ...
+            logger.info(f"Should be good. \n \t URL: {url} \
+                \n\t File: {OUPUTFILE2} \
+                \n\t Output: {VERBOSE}"
+            )
+        
+            logger.info(str(obj))
+            return_status = 0
+        else: 
+            logger.error("Failed to retrieve content from URL.")
+            logger.info("6")
+            return_status = -1
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        logger.error("An error occurred in main", exc_info=True)
 
-    # try:
-    #     url: str = str(sys.argv[1]) + ".html"
-    # except:
-    #     url = "https://en.wikipedia.org/wiki/foo"
-    except:
-        print("some error and the exception")
+# endregion Main 
 
-    # url_arg = ""
-
-    #     # Actually perform the input validation
-    #     # TODO: change signature to validateInput(url, outputFile)
-    #   isValid = validateInput(url, outputFile)
-
-    #     # Separate the messaging
-    #     if isValid != 0:
-    #       # TODO: change signature to displayInputError(isValid, url, outputFile)
-    #         displayInputError(isValid, url, outputFile)
-    #         sys.exit()
-
-    #     # Should be good to go
-    #     obj: requests.Response = getURLObj(url)
-
-    #     if obj:  # Only write if content was successfully retrieved
-    #         writeObj(obj, outputFile)
-    #         # Success ...
-    #         print(
-    #             f"Should be good. \n \t URL: {url} \
-    #             \n\t File: {outputFile} \
-    #             \n\t Output: {verbose}"
-    #         )
-    #         return_status = 0
-    #         # dump the file name
-    #         # messaging(fname) for piping to next step
-    #     else: 
-    #         print("Failed to retrieve content from URL.")
-    #         return_status = -1
-
-
-# endregion
-
+# region  Entry
 
 if __name__ == "__main__":
     """_summary_
@@ -341,16 +270,18 @@ if __name__ == "__main__":
     try:
         logFile = sys.argv[0] + ".log" if sys.argv[0] else "foo" + ".log"
         logging.basicConfig(
-            filename=logFile, format="%(asctime)s %(message)s", filemode="w"
+            filename=logFile, format="%(asctime)s %(message)s", filemode="a"
         )
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
-
+        logger.info("__name __ Started. Calling main()")
+        
         main()
 
     except Exception as e:
-        print("Beyond fubar: " + str(type(e)) + " " + str(e) + " " + str(e.args))
+        logger.critical("Beyond fubar: " + str(type(e)) + " " + str(e) + " " + str(e.args))
 
     finally:
-        print("Done")
-#endregion Main 
+        logger.info("Done")
+
+#endregion Entry 
